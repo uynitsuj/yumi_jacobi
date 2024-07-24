32d9fbdf2e9452c120086b80826627e8b455a775
Justin <yujustin8@gmail.com>
2024-07-24 14:26:30 -0700

    better abstraction

--- starter_examples/test_interface.py
+++ starter_examples/test_interface.py
@@ -3,7 +3,7 @@ from autolab_core import RigidTransform, Point
 # import asyncio
 
 def run():
-    interface = Interface(speed=0.23, file='/home/justinyu/multicable-decluttering/yumi_jacobi/starter_examples/AUTOLAB_BWW_YuMi.jacobi-project')
+    interface = Interface(speed=0.30, file='/home/justinyu/multicable-decluttering/yumi_jacobi/starter_examples/AUTOLAB_BWW_YuMi.jacobi-project')
     print(interface.Frame2RT(interface.get_FK('left')))
     print(interface.Frame2RT(interface.get_FK('right')))
     interface.home()
@@ -16,7 +16,9 @@ def run():
         translation=[0.4, 0.3, 0.2]
     )
     wp2_l = RigidTransform(
-        rotation=[[-1, 0, 0], [0, 1, 0], [0, 0, -1]],
+        rotation=[[-1.0000000, -0.0000000,  0.0000000], 
+                  [-0.0000000,  0.9659258, -0.2588190], 
+                  [-0.0000000, -0.2588190, -0.9659258]],
         translation=[0.4, 0.4, 0.1]
     )
     wp3_l = RigidTransform(
@@ -25,26 +27,32 @@ def run():
     )
     wp1_r = RigidTransform(
         rotation=[[-1, 0, 0], [0, 1, 0], [0, 0, -1]],
-        translation=[0.3, -0.3, 0.15]
+        translation=[0.3, -0.2, 0.15]
     )
     wp2_r = RigidTransform(
         rotation=[[-1, 0, 0], [0, 1, 0], [0, 0, -1]],
-        translation=[0.45, -0.15, 0.2]
+        translation=[0.35, -0.05, 0.2]
     )
     wp3_r = RigidTransform(
-        rotation=[[-1, 0, 0], [0, 1, 0], [0, 0, -1]],
-        translation=[0.5, -0.1, 0.25]
+        rotation=[[-0.7071068,  0.7071068,  0.0000000], 
+                  [0.5000000,  0.5000000,  0.7071068], 
+                  [0.5000000,  0.5000000, -0.7071068]],
+        translation=[0.4, -0.1, 0.15]
     )
     interface.go_cartesian_waypoints(l_targets = [wp1_l, wp2_l, wp3_l], r_targets=[wp1_r, wp2_r, wp3_r])
+    interface.go_linear_single(l_target=wp1_l, r_target=wp1_r)
+    interface.go_linear_single(l_target=wp2_l, r_target=wp2_r)
+    interface.go_linear_single(l_target=wp3_l, r_target=wp3_r)
+    # print(interface.get_FK('left'))
+    # print(interface.get_FK('right'))
     interface.home()
     
     interface.go_delta([0, 0, -0.1])
     interface.go_delta([0, -0.1, 0], [0, 0.1, 0])
-    # print(interface.Frame2RT(interface.get_FK('left')))
-    # print(interface.Frame2RT(interface.get_FK('right')))
     interface.go_delta([0.1, -0.1, 0], [0.1, 0.1, 0])
+    # print(interface.get_FK('left'))
+    # print(interface.get_FK('right'))
     interface.home()
-    # await interface.go_linear(l_target=)
 
 if __name__ == '__main__':
     run()
--- yumi_jacobi/interface.py
+++ yumi_jacobi/interface.py
@@ -1,6 +1,6 @@
 import numpy as np
 from autolab_core import RigidTransform, Point
-from typing import List, Tuple, Union
+from typing import List, Tuple, Union, Literal
 
 from yumi_jacobi.yumi import YuMiArm
 import asyncio
@@ -31,45 +31,88 @@ class Interface:
         return future.result()
 
     def calibrate_grippers(self):
+        ''' 
+        Calibrate both grippers
+        '''
         return self._run_coroutine(self._async_interface.calibrate_grippers())
 
-    def close_grippers(self):
-        return self._run_coroutine(self._async_interface.close_grippers())
+    def close_grippers(self, side: Literal['both','left','right'] = 'both'):
+        '''
+        Close grippers on the specified side ['both','left','right']
+        default = 'both'
+        '''
+        return self._run_coroutine(self._async_interface.close_grippers(side))
 
-    def open_grippers(self):
-        return self._run_coroutine(self._async_interface.open_grippers())
+    def open_grippers(self, side: Literal['both','left','right'] = 'both'):
+        '''
+        Open grippers on the specified side ['both','left','right']
+        default = 'both'
+        '''
+        return self._run_coroutine(self._async_interface.open_grippers(side))
 
-    def grippers_move_to(self, left_dist=0, right_dist=0):
+    def grippers_move_to(self, left_dist: int = 0, right_dist: int = 0):
+        '''
+        Jog grippers to the specified width [0, 25] (mm)
+        Default arguments are 0 mm open grippers
+        '''
         return self._run_coroutine(self._async_interface.grippers_move_to(left_dist, right_dist))
 
     def home(self):
+        '''
+        Move both arms to the home position 
+        '''
         return self._run_coroutine(self._async_interface.home())
 
-    def move_to(self, left_goal=None, right_goal=None):
+    def move_to(self, left_goal: List = None, right_goal: List = None):
+        '''
+        Move both arms to the specified joint angles (radians)
+        '''
         return self._run_coroutine(self._async_interface.move_to(left_goal, right_goal))
 
     def go_delta(self, left_delta=[0, 0, 0], right_delta=[0, 0, 0]):
+        '''
+        Move both arms by the specified cartesian delta [x,y,z], meant for small motions
+        '''
         return self._run_coroutine(self._async_interface.go_delta(left_delta, right_delta))
 
     def go_cartesian_waypoints(self, l_targets=[], r_targets=[]):
+        '''
+        Move both arms to waypoint [RigidTransform] or along specified waypoints (list of RigidTransforms)
+        Currently Jacobi supports up to 3 waypoints
+        '''
         return self._run_coroutine(self._async_interface.go_cartesian_waypoints(l_targets, r_targets))
 
     def go_linear_single(self, l_target=None, r_target=None):
+        '''
+        Move both arms linearly to the specified waypoint
+        '''
         return self._run_coroutine(self._async_interface.go_linear_single(l_target, r_target))
 
-    def get_FK(self, arm):
+    def get_FK(self, arm: Literal['left','right']) -> RigidTransform:
+        '''
+        Get the forward kinematics of the specified arm, returns a RigidTransform
+        '''
         return self._async_interface.get_FK(arm)
 
-    def get_joint_positions(self, arm):
+    def get_joint_positions(self, arm: Literal['left','right']):
+        '''
+        Get the joint positions of the specified arm, returns a list of joint angles (radians)
+        '''
         return self._async_interface.get_joint_positions(arm)
 
     def set_TCP(self):
         return self._async_interface.set_TCP()
 
-    def RT2Frame(self, transform):
+    def RT2Frame(self, transform: RigidTransform) -> Frame:
+        '''
+        Convert RigidTransform (autolab_core) to Frame (Jacobi object)
+        '''
         return self._async_interface.RT2Frame(transform)
 
-    def Frame2RT(self, frame):
+    def Frame2RT(self, frame: Frame) -> RigidTransform:
+        '''
+        Convert Frame (Jacobi object) to RigidTransform (autolab_core)
+        '''
         return self._async_interface.Frame2RT(frame)
 
 class AsyncInterface:
@@ -78,11 +121,6 @@ class AsyncInterface:
     GRIP_DOWN_L = np.diag([-1, 1, -1])
     GRIP_UP_R = np.diag([1, 1, 1])
     GRIP_SIDEWAYS = np.array([[1, 0, 0], [0, 0, 1], [0, -1, 0]])
-    GRIP_SIDEWAYS_L = RigidTransform.x_axis_rotation(-np.pi / 2) @ GRIP_DOWN_R
-    GRIP_TILT_R = RigidTransform.x_axis_rotation(np.pi / 4) @ RigidTransform.z_axis_rotation(np.pi / 2) @ GRIP_DOWN_R
-    GRIP_TILT_L = RigidTransform.x_axis_rotation(-np.pi / 4) @ RigidTransform.z_axis_rotation(np.pi / 2) @ GRIP_DOWN_R
-    GRIP_TILT_R_Y = RigidTransform.y_axis_rotation(-np.pi / 8) @ RigidTransform.z_axis_rotation(np.pi / 2) @ GRIP_DOWN_R
-    GRIP_TILT_L_Y = RigidTransform.y_axis_rotation(np.pi / 8) @ RigidTransform.z_axis_rotation(np.pi / 2) @ GRIP_DOWN_R
     L_ARMS_CLEAR_STATE = np.array([
         -0.5810662,
         -1.34913424,
@@ -124,7 +162,7 @@ class AsyncInterface:
                 self.yumi = self.environment.get_robot()
                 self.yumi.set_speed(speed)
                 self.studio = Studio()
-                self.visualize = False
+                self.visualize = False # Visualizing robot motion in Jacobi Studio causes a pause in execution, so by default it's off
         else:
             self.yumi = Yumi()
             self.yumi.set_speed(speed)
@@ -157,21 +195,17 @@ class AsyncInterface:
         self.yumi.left.flange_to_tcp = self.RT2Frame(self.l_tcp)
         self.yumi.right.flange_to_tcp = self.RT2Frame(self.r_tcp)
 
-    def get_FK(self, arm: str):
+    def get_FK(self, arm: Literal['left','right']):
         if arm == 'left':
-            return self.yumi.left.calculate_tcp(self.driver_left.current_joint_position)
+            return self.Frame2RT(self.yumi.left.calculate_tcp(self.driver_left.current_joint_position))
         elif arm == 'right':
-            return self.yumi.right.calculate_tcp(self.driver_right.current_joint_position)
-        else:
-            raise ValueError(f"Invalid arm {arm}, enter 'left' or 'right'")
+            return self.Frame2RT(self.yumi.right.calculate_tcp(self.driver_right.current_joint_position))
     
-    def get_joint_positions(self, arm: str):
+    def get_joint_positions(self, arm: Literal['left','right']):
         if arm == 'left':
             return self.driver_left.current_joint_position
         elif arm == 'right':
             return self.driver_right.current_joint_position
-        else:
-            raise ValueError(f"Invalid arm {arm}, enter 'left' or 'right'")
         
     async def calibrate_grippers(self):
         await self.driver_left.calibrate_gripper(sync=True)
@@ -184,23 +218,35 @@ class AsyncInterface:
         await asyncio.sleep(1)
         return True
     
-    async def close_grippers(self):
-        await self.driver_left.close_gripper(sync=True)
-        await asyncio.sleep(0.1)
-        await self.driver_right.close_gripper()
-        await asyncio.sleep(1)
+    async def close_grippers(self, side: Literal['both','left','right'] = 'both'):
+        if side == 'both':
+            await self.driver_left.close_gripper(sync=True)
+            await asyncio.sleep(0.1)
+            await self.driver_right.close_gripper()
+            await asyncio.sleep(1)
+        elif side == 'left':
+            await self.driver_left.close_gripper()
+            await asyncio.sleep(0.5)
+        elif side == 'right':
+            await self.driver_right.close_gripper()
+            await asyncio.sleep(0.5)
         return True
     
-    async def open_grippers(self):
-        await self.driver_left.open_gripper(sync=True)
-        await asyncio.sleep(0.1)
-        await self.driver_right.open_gripper()
-        await asyncio.sleep(1)
+    async def open_grippers(self, side: Literal['both','left','right'] = 'both'):
+        if side == 'both':
+            await self.driver_left.open_gripper(sync=True)
+            await asyncio.sleep(0.1)
+            await self.driver_right.open_gripper()
+            await asyncio.sleep(1)
+        elif side == 'left':
+            await self.driver_left.open_gripper()
+            await asyncio.sleep(0.5)
+        elif side == 'right':
+            await self.driver_right.open_gripper()
+            await asyncio.sleep(0.5)
         return True
 
     async def grippers_move_to(self, left_dist = 0, right_dist = 0):
-        # Jog grippers to the specified width [0, 25] (mm)
-        # Default arguments are 0 mm open grippers
         await self.driver_left.move_gripper(left_dist, sync=True)
         await asyncio.sleep(0.1)
         await self.driver_right.move_gripper(right_dist)
@@ -210,18 +256,17 @@ class AsyncInterface:
     async def home(self):
         l_pos = self.yumi.left.calculate_tcp(self.driver_left.current_joint_position)
         r_pos = self.yumi.right.calculate_tcp(self.driver_right.current_joint_position)
+        self.yumi.set_speed(0.2)
         if l_pos.translation[2] < .08 or r_pos.translation[2] < .08:
-            self.yumi.set_speed(0.1)
             await self.go_delta([0, 0, 0.12 - l_pos.translation[2]],
                                 [0, 0, 0.12 - r_pos.translation[2]])
-        self.yumi.set_speed(self.speed)
 
         result_left, result_right = await self.move_to(self.L_ARMS_CLEAR_STATE, self.R_ARMS_CLEAR_STATE)
+        self.yumi.set_speed(self.speed)
         print("Homing done")
         return result_left, result_right
     
     async def move_to(self, left_goal = None, right_goal=None):
-        # Move both arms to the specified joint angles (radians)
         result_left = self.driver_left.move_to_async(left_goal)
         result_right = self.driver_right.move_to_async(right_goal)
         await result_left
@@ -229,7 +274,7 @@ class AsyncInterface:
         return result_left, result_right
     
     async def go_delta(self, left_delta: List = [0, 0, 0], right_delta: List = [0, 0, 0]):
-        # Move both arms by the specified cartesian delta [x,y,z], meant for small motions
+        assert len(left_delta) == 3 and len(right_delta) == 3, "Delta must be a list of 3 elements"
         l_curr_pos = self.yumi.left.calculate_tcp(self.driver_left.current_joint_position)
         l_goal_mat = l_curr_pos.matrix
         l_goal_mat[-4:-1] += np.array(left_delta)
@@ -263,9 +308,6 @@ class AsyncInterface:
         return result_left, result_right
 
     async def go_cartesian_waypoints(self, l_targets: List[RigidTransform]=[], r_targets: List[RigidTransform]=[]):
-        # Move both arms to waypoint [RigidTransform] or along specified waypoints (list of RigidTransforms)
-        # Currently Jacobi supports up to 3 waypoints
-        # Currently left executes before right arm; looking into simultaneous execution
         assert (len(l_targets) > 0 or len(r_targets) > 0), "No waypoints provided"
         if len(l_targets) > 0:
             motion = self.listRT2Motion(
@@ -300,17 +342,17 @@ class AsyncInterface:
         return result_left, result_right
 
     async def go_linear_single(self, l_target: RigidTransform=None, r_target: RigidTransform=None):
-        # Move both arms linearly to the specified waypoint
-        assert (l_target is not None or r_target is not None), "No waypoints provided"
+        assert (l_target is not None or r_target is not None), "No linear targets provided"
         if l_target is not None:
             motion = LinearMotion(
                 robot = self.yumi.left,
                 start = self.driver_left.current_joint_position,
                 goal = self.RT2Frame(l_target)
             )
-            trajectory = self.planner.plan(motion)
-            if self.studio is not None:
-                self.studio.run_trajectory(trajectory)
+            start_time = time.time()
+            trajectory_l = self.planner.plan(motion)
+            elapsed_time = (time.time() - start_time) * 1000 
+            print(f"[go_linear_single] Time to plan left arm waypoints: {elapsed_time:.3f} ms")
                 
         if r_target is not None:
             motion = LinearMotion(
@@ -318,12 +360,17 @@ class AsyncInterface:
                 start = self.driver_right.current_joint_position,
                 goal = self.RT2Frame(r_target)
             )
-            trajectory = self.planner.plan(motion)
-            if self.studio is not None:
-                self.studio.run_trajectory(trajectory)
+            start_time = time.time()
+            trajectory_r = self.planner.plan(motion)
+            elapsed_time = (time.time() - start_time) * 1000 
+            print(f"[go_linear_single] Time to plan right arm waypoints: {elapsed_time:.3f} ms")
+            
+        if self.studio is not None and self.visualize:
+            self.studio.run_trajectory(trajectory_l)
+            self.studio.run_trajectory(trajectory_r)
         
-        result_left = self.driver_left.run_async(trajectory)
-        result_right = self.driver_right.run_async(trajectory)
+        result_left = self.driver_left.run_async(trajectory_l)
+        result_right = self.driver_right.run_async(trajectory_r)
         await result_right
         await result_left
         return result_left, result_right
@@ -331,7 +378,6 @@ class AsyncInterface:
 
     def listRT2Motion(self, robot, start: List, wp_list: List[RigidTransform]) -> Motion:
         # Convert list of RigidTransform to Motion (Jacobi object) 
-        # Currently Jacobi supports up to 2 waypoints
         motion = Motion(
             robot, 
             start,
